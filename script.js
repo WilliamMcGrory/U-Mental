bb8873e76607b6d66fc3cbb543b7f96996ec6a56
WilliamMcGrory <willi.mc@hotmail.com>
2021-08-18 20:38:11 +0200

    j

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const posts = [
   description: "<p>fjerde side side</p>",
   image: "https://image.ibb.co/cTfZVk/4.jpg",
   clip_path: "polygon(24% 0, 90% 30%, 79% 78%, 13% 76%)",
-  Link: "https://www.youtube.com/embed/5aOMZ5Ctgr8" 
+  Link: "https://www.youtube.com/embed/NykgxTtJxNM"
 },
 
 {
