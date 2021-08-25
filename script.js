7b115ea3f647905c0b09c47d220086696047cf54
WilliamMcGrory <willi.mc@hotmail.com>
2021-08-25 19:32:27 +0200

    d

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const navSlide = () =>{
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-Links');
-const navLinks = document.querySelectorAll('.nav-Links li');
-//Toggle Nav 
-burger.addEventListener('click', () => {
-  nav.classList.toggle ('nav-active');
-});
-  //animate links
-navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-    console.log(index / 5);
-});
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-Links');
+    const navLinks = document.querySelectorAll('.nav-Links li');
+
+
+    burger.addEventListener('click', () => {
+      //Toggle Nav 
+        nav.classList.toggle ('nav-active');
+
+      //animate links
+      navLinks.forEach((link, index) => {
+        if (link.getElementsByClassName.animation){
+          link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      } 
+      });
+      //burger animation
+      burger.classList.toggle('toggle');
+  });
 }
 
 navSlide();
