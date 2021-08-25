ecd39a5cc41f00eb60c9a6cbae196ae0dab628c2
WilliamMcGrory <willi.mc@hotmail.com>
2021-08-25 15:12:11 +0200

    f

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ burger.addEventListener('click', () => {
 });
   //animate links
 navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards s (index / 7}`;
-    console.log(index / 7);
-    
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    console.log(index / 5);
 });
 }
 
