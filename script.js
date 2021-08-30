17cb6f0ee516b9b976c2eb0e88784965bdae65b7
WilliamMcGrory <willi.mc@hotmail.com>
2021-08-30 16:16:30 +0200

    d

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const navSlide = () =>{
+const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-Links')
     const navLinks = document.querySelectorAll('.nav-Links li');
 
 
-    burger.addEventListener('click',()=>{
+    burger.addEventListener('click', () => {
       //Toggle Nav 
         nav.classList.toggle ('nav-active');
 
       //animate links
       navLinks.forEach((link, index) => {
-        if (link.getElementsByClassName.animation){
-          link.style.animation = ''
+        if (link.style.animation) {
+          link.style.animation = '';
         } else {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
       } 
       });
       //burger animation
       burger.classList.toggle('toggle');
-  });
+    });
 }
 
 navSlide();
