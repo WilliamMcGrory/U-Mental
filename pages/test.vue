c2e5975378ed123309801590e872a6e230762438
WilliamMcGrory <willi.mc@hotmail.com>
2021-09-05 16:42:40 +0200

    nuxt
    nuxt

---
+++ pages/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>YO</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
+
