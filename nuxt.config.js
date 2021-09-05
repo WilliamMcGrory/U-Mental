fe1a71ab621687a8bf3c2999c1a5813896b8b116
WilliamMcGrory <willi.mc@hotmail.com>
2021-09-05 16:34:05 +0200

    Nuxt
    Nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/eksempel'
+  }
+} : {}
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'eksempel',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/eksempel/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  ...routerBase,
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+}
+
