566a803315933abd2f2e22b2fa7db8fe76363682
WilliamMcGrory <willi.mc@hotmail.com>
2021-09-18 22:26:39 +0200

    last Fixed

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -134,7 +134,7 @@ export async function setContext(app, context) {
 
       payload: context.payload,
       error: context.error,
-      base: '/',
+      base: '/eksempel',
       env: {}
     }
     // Only set once
